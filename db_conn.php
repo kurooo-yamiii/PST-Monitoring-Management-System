1f80415d0e9b819f7eb56e133fe83d2a4ca7ccc5
KC <134997423+kurooo-yamiii@users.noreply.github.com>
2024-10-25 21:54:54 +0800

    Restrict Connection

--- db_conn.php
+++ db_conn.php
@@ -1,10 +1,10 @@
 <?php
 // Getting Connection to the published Database
-$sname= "fdb1034.awardspace.net";
-$unmae= "4394207_php";
-$password = "rtupst@123";
+$sname= "";
+$unmae= "";
+$password = "";
 
-$db_name = "4394207_php";
+$db_name = "";
 
 $conn = mysqli_connect($sname, $unmae, $password, $db_name);
 
